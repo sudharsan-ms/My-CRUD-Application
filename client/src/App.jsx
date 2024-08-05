e43eed074f3c9692b8466fbbbab5f55489634959
sudharsan-ms <mssudharsanar@gmail.com>
2024-08-05 14:00:16 +0530

    modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import './App.css';
 function App() {
   const [users, setusers] = useState([]);
   const [filterUsers, setfilterUsers]= useState([]);
+  const [isModel, setisModel] = useState(false);
+  const [userData, setuserData] = useState({name:"",age:"",city:""})
 
 const getalluser = async () => {
   await axios.get("http://localhost:8000/users").then
@@ -49,13 +51,63 @@ const handleDelete = async(id) =>{
 
 };
 
+//handle model
+const handleModel=()=>{
+  setisModel(true);
+  setuserData({name:"",age:"",city:""});
+}
+
+//close model
+const closeModel = ()=>{
+  setisModel(false);
+  getalluser();
+}
+
+//handle data
+const handleData = (e) =>{
+setuserData({...userData,
+  [e.target.name]:e.target.value})
+  console.log(e.target.value);
+  
+}
+
+//handle submit
+const handleSubmit = async (e) => {
+ 
+  e.preventDefault();
+  if(userData.id)
+    {await axios.patch(`http://localhost:8000/users/${userData.id}`, userData).then
+    ((res)=>{console.log(res);
+  });
+}
+
+else{
+  await axios.post("http://localhost:8000/users",userData).then((res)=>{
+console.log(res);
+});
+
+closeModel()
+setuserData({name:"",age:"",city:""});
+}}
+
+ 
+//handle edit
+const handleEdit=(user)=>{
+setisModel(true);
+setuserData(user)
+console.log(user);
+
+}
+  
+
+
 return (
     <>
       <div id='container'>
        <h3>CRUD Application with React.js Frontend & Node.js Backend</h3>
        <div className='inputbox'> 
          <input onChange={userSearch} type="search" placeholder='Search Text Here'/>
-         <button className='btn'>Add Reacord</button>
+         <button className='btn' onClick={handleModel}>Add Record</button>
          </div>
          <table className='tablecontainer'>
           <thead> 
@@ -76,15 +128,37 @@ return (
                   <td>{user.name}</td>
                   <td>{user.age}</td>
                   <td>{user.city}</td>
-                  <td><button className="btn">Edit</button></td>
+                  <td><button className="btn" onClick={()=>handleEdit(user)}>Edit</button></td>
                   <td><button onClick={()=>{handleDelete(user.id)}} className="btn red">Delete</button></td>
                 </tr>
                 )
               })} 
                </tbody>
             </table>
+            {isModel && 
+            <div className='model'>
+              <div className='model-content'>
+                <span className='close' onClick={closeModel}>&times;</span>
+                <h2>{userData.id ? "Edit Record" : "Add Record"}</h2>
+                <div className='inputgroup'>
+                  <label htmlFor="name">Name</label>
+                  <input onChange={handleData} type="text" name='name' id='name' value={userData.name} />
+                </div>
+                <div className='inputgroup'>
+                  <label htmlFor="age">Age</label>
+                  <input onChange={handleData} type="number" name='age' id='age' value={userData.age}/>
+                </div>
+                <div className='inputgroup'>
+                  <label htmlFor="city">City</label>
+                  <input onChange={handleData} type="text" name='city' id='city' value={userData.city}/>
+                </div>
+                <div className='inputgroup'><button className='btn green' onClick={handleSubmit}>{userData.id ? "update":"Add"}</button></div>
+              </div>
+              </div>}
        </div>
     </> )
 }
+
+
 
 export default App
--- server/index.js
+++ server/index.js
@@ -2,16 +2,18 @@ const express = require("express");
 const users = require("./sample.json");
 const cors = require("cors");
 const fs = require("fs");
-const { log } = require("console");
+
 
 const app = express();
+app.use(express.json());
+
 const port = 8000;
 
 app.use(
     cors({
     "origin":"*",
     "methods":["GET","POST","PATCH","DELETE"],
-   "preflightContinue":false,
+    "preflightContinue":false,
     "optionsSuccessStatus":204
   
 }));
@@ -20,19 +22,53 @@ app.use(
 app.get("/users",(req,res)=>{
 return res.json(users);
 })
-
+//delete users
 app.delete("/users/:id",(req,res)=>{
     let id = Number(req.params.id)
     let filteredUser= users.filter((user) => user.id !== id );
     console.log( filteredUser);
     fs.writeFile("./sample.json",JSON.stringify(filteredUser),(err,data)=>{
         return res.json(filteredUser);
-      
+      })
     })
-    
+
+    //for add users
+ app.post("/users",(req,res)=>{
+
+ let {name,age,city} = req.body;
+
+  if(!name || !age || !city){
+    res.status(400).send({message:"Fill All Fields"});
+  }
+  let id = Date.now();
+  users.push({id,name,age,city});
+
+  fs.writeFile("./sample.json",JSON.stringify(users),(err,data)=>{
+     return res.json({message: "Successfully Added"});
+  })
+  
 })
+
+app.patch("/users/:id",(req,res)=>{
+  let id = Number(req.params.id);
+  let {name,age,city} = req.body;
+ 
+   if(!name || !age || !city){
+     res.status(400).send({message:"Fill All Fields"});
+   }
+let index = users.findIndex((user)=> user.id === id);
+
+users.splice(index,1,{...req.body})
+ 
+   fs.writeFile("./sample.json",JSON.stringify(users),(err,data)=>{
+      return res.json({message: "Successfully Updated"});
+   })
+   
+ })
+
+
 app.listen(port, (res)=>{
 console.log("Server running on port " + port);
 })
 
-
+ 
